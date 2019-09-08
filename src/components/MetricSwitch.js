6873eee272599b869605f5516e16122647b3da8a
JJ <jj.spetseris@yahoo.com>
2019-09-08 15:32:38 -0500

    switch styling

--- src/components/MetricSwitch.js
+++ src/components/MetricSwitch.js
@@ -3,32 +3,46 @@ import React from 'react';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  MuiFormGroup: {
+    background: theme.palette.background.main,
+    margin: "1em",
+    float: "left",
+    height: "100vh"
+  }
+}));
 
 const MerticSwitch = props => {
   // console.log("props", props.metric)
   const metrics = props.metric.data.getMetrics
+  const classes = useStyles();
 
   const [state, setState] = React.useState(false);
 
   const handleChange = name => event => {
+    // console.log(name, event.target.checked)
     setState({ ...state, [name]: event.target.checked });
   };
 
-  const options = metrics.map( x => {
-    // console.log("line:", x)
+  const options = metrics.map( metric => {
+    // console.log("line:", metric)
     return (
       <FormControlLabel
-        control={
-          <Switch checked={state.x} onChange={handleChange({x})} value={x} />
+        key={metric}
+        control = {
+          <Switch checked={state.metric} onChange={handleChange({metric})} value={metric} color="primary" />
         }
-        label={x}
+        label ={metric}
+        labelPlacement="start"
       />
     )
   })
 
 
   return (
-    <FormGroup>
+    <FormGroup className={classes.MuiFormGroup}>
       {options}
     </FormGroup>
   );
